66ea285f9b2110ee2ea90f877c39c881195622c8
AgaPiotrowska <agniegrabo@gmail.com>
2020-12-13 19:41:24 +0100

    added calendar header mockup

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,9 +1,34 @@
 <template>
-    <div>
-        Calendar
+    <div class="calendar-header">
+        <img class="calendar-arrow" src="/assets/arrow-left.svg"/>
+        <div class="calendar-month text-big ">Calendar</div>
+        <img class="calendar-arrow" src="/assets/arrow-right.svg"/>
     </div>
 </template>
 
 <script>
     export default {}
 </script>
+
+<style>
+    .calendar-header {
+        height: 50px;
+        background-color: #00DBB1;
+        color: white;
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    .calendar-arrow {
+        height: 20px;
+        margin-left: 20px;
+        margin-right: 20px;
+    }
+
+    .calendar-month {
+        flex-grow: 1;
+        text-align: center;
+        color: white;
+    }
+</style>
