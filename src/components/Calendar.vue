1022e8cde43978bacf401647065d742bcaf0d796
AgaPiotrowska <agniegrabo@gmail.com>
2020-12-13 21:50:12 +0100

    show selected start and end days

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -18,9 +18,10 @@
             <div class="calendar-day"
                  v-for="(day, index) in days"
                  v-bind:key="index"
-
+                 @click="selectDay(day)"
             >
-                <div class="calendar-day-inner" v-bind:class="{today: day.isToday, available: day.isAvailable}"
+                <div class="calendar-day-inner"
+                     v-bind:class="{today: day.isToday, available: day.isAvailable, start: day.isStart, end: day.isEnd}"
                 >
                     {{day.day}}
                 </div>
@@ -37,17 +38,29 @@
                 days: [],
                 currentMonth: null,
                 currentDate: new Date(),
-                today: new Date()
+                today: new Date(),
+                startDate: null,
+                endDate: null
             }
         },
         props: ["availableDays"],
         methods: {
+            selectDay(day) {
+                if (this.startDate != null && this.endDate === null) {
+                    this.endDate = new Date(day.fullDate);
+                } else {
+                    this.endDate = null;
+                    this.startDate = new Date(day.fullDate)
+                }
+                this.calculateDays(this.currentDate)
+            },
+
             getPreviousMonth() {
-                this.currentDate = new Date(this.currentDate.setMonth(this.currentDate.getMonth()-1));
+                this.currentDate = new Date(this.currentDate.setMonth(this.currentDate.getMonth() - 1));
                 this.calculateDays(this.currentDate)
             },
             getNextMonth() {
-                this.currentDate = new Date(this.currentDate.setMonth(this.currentDate.getMonth()+1));
+                this.currentDate = new Date(this.currentDate.setMonth(this.currentDate.getMonth() + 1));
                 this.calculateDays(this.currentDate)
             },
 
@@ -60,42 +73,47 @@
                 }
                 const currentDate = getFirstDayOfWeek(firstDayOfMonth);
                 const lastDayOfMonth = getLastDayOfMonth(dayInMonth);
-                console.log(this.availableDays);
-                while(currentDate.getDay() !== 0 || currentDate.getTime() < lastDayOfMonth.getTime()) {
+                while (currentDate.getDay() !== 0 || currentDate.getTime() < lastDayOfMonth.getTime()) {
                     this.days.push({
                         day: currentDate.getDate(),
+                        fullDate: new Date(currentDate),
                         isToday: isSameDays(currentDate, this.today),
-                        isAvailable: this.isDayAvailable(currentDate)
+                        isAvailable: this.isDayAvailable(currentDate),
+                        isStart: isSameDays(currentDate, this.startDate),
+                        isEnd: isSameDays(currentDate, this.endDate)
                     });
-                    currentDate.setDate(currentDate.getDate()+1);
+                    currentDate.setDate(currentDate.getDate() + 1);
 
-                } console.log(this.days)
+                }
             },
             isDayAvailable(dayToCheck) {
-                return this.availableDays.some(day => isSameDays(new Date(day.year, day.month-1, day.day), dayToCheck));
+                return this.availableDays.some(day => isSameDays(new Date(day.year, day.month - 1, day.day), dayToCheck));
             }
         },
         created: function () {
             this.calculateDays(this.currentDate)
         }
     }
-    const isSameDays= (first, second) => {
+    const isSameDays = (first, second) => {
+        if (first === null || second === null) {
+            return false
+        }
         return first.getFullYear() === second.getFullYear() && first.getMonth() === second.getMonth()
             && first.getDate() === second.getDate()
     }
 
-   const getFirstDayOfMonth = (dayInMonth) => {
-       return new Date(dayInMonth.getFullYear(), dayInMonth.getMonth(), 1);
+    const getFirstDayOfMonth = (dayInMonth) => {
+        return new Date(dayInMonth.getFullYear(), dayInMonth.getMonth(), 1);
     };
 
     const getLastDayOfMonth = (dayInMonth) => {
-        return new Date(dayInMonth.getFullYear(), dayInMonth.getMonth()+1, 0);
+        return new Date(dayInMonth.getFullYear(), dayInMonth.getMonth() + 1, 0);
     }
 
     const getFirstDayOfWeek = (date) => {
         const currentDate = new Date(date);
-        while(currentDate.getDay() !== 0) {
-            currentDate.setDate(currentDate.getDate()-1);
+        while (currentDate.getDay() !== 0) {
+            currentDate.setDate(currentDate.getDate() - 1);
         }
         return currentDate;
     }
@@ -184,6 +202,13 @@
 
     .calendar-day-inner.available {
         color: #787676;
+        cursor: pointer;
+    }
+
+    .start, .end {
+        background-color: #00DBB1;
+        color: white !important;
+        border-radius: 50%;
     }
 
 </style>
