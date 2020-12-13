89538ac167dcff12b4cfa3d344138e455958c47d
AgaPiotrowska <agniegrabo@gmail.com>
2020-12-13 17:16:53 +0100

    basic styles and structure added

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,10 @@ export default {
 </script>
 
 <style>
-
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
+}
 </style>
--- src/components/BookingForm.vue
+++ src/components/BookingForm.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>Booking form</div>
+  <div class="booking-container">
+    <div class="border-container">
+      <h2>Price</h2>
+      <div>ratings</div>
+      <div class="border-bottom"></div>
+      <h3 class="dates">Dates</h3>
+      <div class="check-container">
+        <h2>Check In</h2>
+        <img src="../assets/arrow.svg"/>
+        <h2>Check Out</h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +21,39 @@
 </script>
 
 <style>
+
+  .booking-container {
+    height: 450px;
+    width: 450px;
+    background-color: #ECECEC;
+    margin: auto;
+    padding: 15px;
+  }
+
+  .border-container {
+    border: 2px solid #DBDBDB;
+    height: 100%;
+    width: 100%;
+    padding: 15px;
+  }
+
+  .border-bottom {
+    border-bottom: 2px solid #DBDBDB;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  .check-container {
+    display: flex;
+    border: 2px solid #DBDBDB;
+  }
+
+  .check-container h2 {
+    margin: 7px 15px;
+    font-weight: lighter;
+  }
+
+  .check-container img {
+    margin: 0 20px;
+  }
 </style>
